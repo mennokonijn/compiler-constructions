05e6a2f9587622e88961ffae3a1d2fad2fb1cae4
mennokonijn <99171568+mennokonijn@users.noreply.github.com>
2023-03-10 15:25:42 +0100

    Milestone 3 part 2

--- src/print/print.c
+++ src/print/print.c
@@ -11,6 +11,8 @@
 #include "ccngen/ast.h"
 #include "ccngen/trav.h"
 #include "palm/dbug.h"
+#include "user_types.h"
+
 
 /**
  * @fn PRTstmts
@@ -101,6 +103,23 @@ node_st *PRTbinop(node_st *node)
     printf( ")(%d:%d-%d)", NODE_BLINE(node), NODE_BCOL(node), NODE_ECOL(node));
 
     return node;
+}
+
+const char *getTypeString(valType type) {
+    switch (type) {
+    case CT_void:
+        return "void";
+    case CT_bool:
+        return "bool";
+    case CT_int:
+        return "int";
+    case CT_float:
+        return "float";
+    default:
+        return "NULL";
+  }
+
+  return "unknown";
 }
 
 /**
@@ -270,6 +289,9 @@ node_st *PRTcast(node_st *node)
  */
 node_st *PRTfundefs(node_st *node)
 {
+    TRAVfundef(node);
+    printf("\n");
+    TRAVnext(node);
     return node;
 }
 
@@ -278,6 +300,14 @@ node_st *PRTfundefs(node_st *node)
  */
 node_st *PRTfundef(node_st *node)
 {
+    if (FUNDEF_EXPORT(node) != NULL) {
+        printf('export ');
+    }
+    printf("%s %s (", getTypeString(FUNDEF_TYPE(node)), FUNDEF_NAME(node));
+    TRAVparams(node);
+    printf(") \n {");
+    TRAVbody(node);
+    printf("\n}");
     return node;
 }
 
@@ -286,6 +316,9 @@ node_st *PRTfundef(node_st *node)
  */
 node_st *PRTfunbody(node_st *node)
 {
+    TRAVdecls(node);
+    TRAVstmts(node);
+    TRAVlocal_fundefs(node);
     return node;
 }
 
@@ -294,6 +327,17 @@ node_st *PRTfunbody(node_st *node)
  */
 node_st *PRTifelse(node_st *node)
 {
+    printf("if (");
+    TRAVcond(node);
+    printf(") \n{");
+    TRAVthen(node);
+    print("} \n");
+    if (IFELSE_ELSE_BLOCK(node) != NULL) {
+        printf("else {");
+        TRAVelse_block(node);
+        printf("} \n");
+    }
+
     return node;
 }
 
@@ -302,6 +346,12 @@ node_st *PRTifelse(node_st *node)
  */
 node_st *PRTwhile(node_st *node)
 {
+    printf("while (");
+    TRAVcond(node);
+    printf(") \n{");
+    TRAVblock(node);
+    printf("\n}");
+
     return node;
 }
 
@@ -325,6 +375,16 @@ return node;
  */
 node_st *PRTfor(node_st *node)
 {
+    printf("for ( int %s = ", FOR_VAR(node));
+    FOR_START_EXPR(node);
+    printF(", ");
+    FOR_STOP(node);
+    printF(", ");
+    FOR_STEP(node);
+    printf(") \n{");
+    TRAVblock(node);
+    printf("\n}");
+
     return node;
 }
 
@@ -333,6 +393,11 @@ node_st *PRTfor(node_st *node)
  */
 node_st *PRTglobdecl(node_st *node)
 {
+    printf("extern ");
+    printf("%s %s", getTypeString(GLOBDECL_TYPE(node)), GLOBDECL_NAME(node));
+    TRAVdims(node);
+
+    printf(";")
     return node;
 }
 
@@ -341,6 +406,17 @@ node_st *PRTglobdecl(node_st *node)
  */
 node_st *PRTglobdef(node_st *node)
 {
+
+    if (GLOBDEF_EXPORT(node) == true) {
+        printf("export ");
+    }
+    printf("%s %s", getTypeString(GLOBDEF_TYPE(node)), GLOBDEF_NAME(node));
+    TRAVdims(node);
+    if (GLOBDEF_INIT(node) != NULL) {
+        printf(" = ");
+        TRAVinit(node);
+    }
+    printf(";")
     return node;
 }
 
@@ -349,6 +425,14 @@ node_st *PRTglobdef(node_st *node)
  */
 node_st *PRTparam(node_st *node)
 {
+    printf("%s %s", getTypeString(PARAM_TYPE(node)), PARAM_NAME(node));
+
+    TRAVdims(node);
+
+    if (PARAM_NEXT(node) != NULL) {
+        printf(', ')
+        TRAVnext(node);
+    }
     return node;
 }
 
@@ -357,6 +441,20 @@ node_st *PRTparam(node_st *node)
  */
 node_st *PRTvardecl(node_st *node)
 {
+    printf("%s %s", getTypeString(VARDECL_TYPE(node)), VARDECL_NAME(node));
+    TRAVdims(node);
+
+    if (VARDECL_INIT(node) != null) {
+        printf(" = ")
+        TRAVinit(node);
+    }
+
+    if (VARDECL_NEXT(node) != null) {
+        print(", ")
+        TRAVnext(node);
+    }
+    printf(";")
+
     return node;
 }
 
@@ -365,6 +463,18 @@ node_st *PRTvardecl(node_st *node)
  */
 node_st *PRTmonop(node_st *node)
 {
+
+   switch (MONOP_OP(node)) {
+    case MO_neg:
+      tmp = "-";
+      break;
+    case MO_not:
+      tmp = "!";
+      break;
+    case MO_NULL:
+      DBUG_ASSERT(false, "unknown monop detected!");
+    }
+
+    printf( "%s", tmp);
     return node;
 }
-
--- src/user_types.h
+++ src/user_types.h
@@ -3,3 +3,11 @@
 #include "palm/hash_table.h"
 
 typedef htable_st *htable_ptr;
+
+typedef enum {
+    CT_NULL,
+    CT_int,
+    CT_float,
+    CT_bool,
+    CT_void,
+} valType;
